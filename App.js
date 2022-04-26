34094197fffc61e9aaf48f9f45a18207d9c89804
MarioRMN <mariosfcroman@gmail.com>
2022-04-26 13:29:37 +0200

    Contenedores y cambios

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function App(){
-return(
-  <View>
-    <Text>Hola</Text>
-  </View>
-);
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const Tab = createBottomTabNavigator();
+
+
+import HistoriaPantalla from "./src/containers/HistoriaPantalla";
+import PanesPantalla from "./src/containers/PanesPantalla";
+
+
+export default function App({navigation}) {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'HISTORIA') {
+                iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'PANES') {
+                iconName = focused ? 'paw' : 'paw-outline';
+              }
+              
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'grey',
+          })}
+        >
+        <Tab.Screen options={{ headerShown: false }} name="HISTORIA" component={HistoriaPantalla} />
+        <Tab.Screen options={{ headerShown: false }} name="PANES" component={PanesPantalla} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App;
